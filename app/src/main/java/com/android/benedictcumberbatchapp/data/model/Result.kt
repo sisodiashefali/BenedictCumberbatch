7f5da982b929e203def1ad352fc7b0b6fec60acd
Shefali <sisodiashefali27@gmail.com>
2024-07-29 07:38:04 -0400

    test

--- app/src/main/java/com/android/benedictcumberbatchapp/data/model/Result.kt
+++ app/src/main/java/com/android/benedictcumberbatchapp/data/model/Result.kt
@@ -7,14 +7,14 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Result(
     val adult: Boolean,
-    @SerializedName("backdrop_path") val backdropPath: String? ,
+    @SerializedName("backdrop_path") val backdropPath: String ,
     @SerializedName("genre_ids") val genreIds: List<Int>,
     @SerializedName("id") val id: Int,
     @SerializedName("original_language") val originalLanguage: String,
     @SerializedName("original_title") val originalTitle: String,
     @SerializedName("overview") val overview: String,
     @SerializedName("popularity") val popularity: Double,
-    @SerializedName("poster_path") val posterPath: String?,
+    @SerializedName("poster_path") val posterPath: String,
     @SerializedName("release_date") val releaseDate: String,
     @SerializedName("title") val title: String,
     @SerializedName("video") val video: Boolean,
